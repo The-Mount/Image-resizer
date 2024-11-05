9dfe5ae597c0c02b38efb888150ca6d1d601a3fb
Dareos Meyer <dameyer@msmu.edu>
2024-11-05 10:43:12 -0800

    added indicator for pending images and cropped images

--- smartcrop+dropdown.js
+++ smartcrop+dropdown.js
@@ -7,7 +7,9 @@ const applyResolutionButton = document.getElementById("apply-resolution");
 const customResWidth = document.getElementById("width");
 const customResHeight = document.getElementById("height");
 const loader = document.querySelector(".loader");
-let rawImages;
+const fileCounter = document.querySelector(".file-counter");
+const fileCounterContent = document.querySelector(".file-counter p");
+let rawImages = [];
 let croppedImages = [];
 let cropSize = {
   "width": null,
@@ -26,8 +28,12 @@ dropArea.addEventListener("dragleave", () => dropArea.classList.remove("drag-ove
 dropArea.addEventListener("drop", (e) => {
   e.preventDefault();
   dropArea.classList.remove("drag-over");
+  // Array.from(e.dataTransfer.files).forEach(file => {
+  //   rawImages.push(file);
+  // });
   rawImages = e.dataTransfer.files;
   resolutionSelect.classList.remove('hidden')
+  handleEnableFileCounter(rawImages);
 });
 
 function handleFiles(files, cropSize) {
@@ -39,9 +45,22 @@ function handleFiles(files, cropSize) {
 }
 
 function handleLoader() {
+  console.log(croppedImages.length);
+  console.log(rawImages.length);
   if (croppedImages.length >= rawImages.length) {
     loader.classList.add('hidden');
   }
+}
+
+function handleEnableFileCounter(files) {
+  fileCounterContent.innerText = `${files.length} image(s) ready for cropping`;
+  fileCounter.style.backgroundColor = "#6cc5e9"
+  fileCounter.classList.remove("hidden");
+}
+
+function handleDisableFileCounter(files) {
+  fileCounterContent.innerText = `${files.length} image(s) cropped!`;
+  fileCounter.style.backgroundColor = "#94d600"
 }
 
 function processFile(file, cropSize) {
@@ -59,7 +78,7 @@ function processFile(file, cropSize) {
 }
 
 function handleUserEntry() {
-  if (customResWidth.value && customResHeight.value && resolutionDropdown.value) {
+  if (resolutionDropdown.value && customResWidth.value || customResHeight.value) {
     alert("Please use only the dropdown or a custom size");
   } else if (customResWidth.value && customResHeight.value) {
     cropSize.width = customResWidth.value;
@@ -86,7 +105,7 @@ applyResolutionButton.onclick = () => {
 };
 
 // Create preview with Smartcrop for cropping accuracy
-function createPreview(imageSrc, file, cropSize) {  
+function createPreview(imageSrc, file, cropSize) {
   const img = new Image();
   img.src = imageSrc;
   img.onload = () => {
@@ -125,43 +144,48 @@ function createPreview(imageSrc, file, cropSize) {
       downscaleCanvas.toBlob((blob) => {
         const croppedImageURL = URL.createObjectURL(blob);
         croppedImages.push({ name: file.name, blob });
+        createListItem(file, img, croppedImageURL, blob);
         handleLoader();
-        const listItem = document.createElement("li");
-        const croppedImage = document.createElement("img");
-        const imageInfoContainer = document.createElement("div");
-        const imageSizeInfo = document.createElement("p");
-        const imageNameInfo = document.createElement("p");
-        imageNameInfo.classList.add("image-name");
-        imageNameInfo.innerText = file.name;
-        imageSizeInfo.innerText = `${img.width}x${img.height}px → ${cropSize.width}x${cropSize.height}px`
-        croppedImage.src = croppedImageURL;
-        imageInfoContainer.appendChild(imageNameInfo);
-        imageInfoContainer.appendChild(imageSizeInfo);
-        listItem.appendChild(croppedImage);
-        listItem.appendChild(imageInfoContainer);
-
-        const downloadButton = document.createElement("button");
-        downloadButton.classList.add("download-button");
-        const downloadIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        const downloadIconPath1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        const downloadIconPath2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-
-        downloadIcon.setAttribute("viewBox", "0 0 29.978 29.978");
-        downloadIcon.setAttribute("width", "20");
-        downloadIcon.setAttribute("height", "20");
-        downloadIconPath1.setAttribute("d", "M25.462,19.105v6.848H4.515v-6.848H0.489v8.861c0,1.111,0.9,2.012,2.016,2.012h24.967c1.115,0,2.016-0.9,2.016-2.012v-8.861H25.462z");
-        downloadIconPath2.setAttribute("d","M14.62,18.426l-5.764-6.965c0,0-0.877-0.828,0.074-0.828s3.248,0,3.248,0s0-0.557,0-1.416c0-2.449,0-6.906,0-8.723c0,0-0.129-0.494,0.615-0.494c0.75,0,4.035,0,4.572,0c0.536,0,0.524,0.416,0.524,0.416c0,1.762,0,6.373,0,8.742c0,0.768,0,1.266,0,1.266s1.842,0,2.998,0c1.154,0,0.285,0.867,0.285,0.867s-4.904,6.51-5.588,7.193C15.092,18.979,14.62,18.426,14.62,18.426z");
-        downloadIcon.appendChild(downloadIconPath1);
-        downloadIcon.appendChild(downloadIconPath2);
-        downloadButton.appendChild(downloadIcon);
-        downloadButton.onclick = () => downloadImage(blob, file.name);
-        listItem.appendChild(downloadButton);
-        downloadAllButton.classList.remove('hidden');
-
-        previewList.appendChild(listItem);
+        handleDisableFileCounter(croppedImages)
       }, "image/jpeg", 0.9); // Set JPEG quality to 90%
     });
   };
+}
+
+function createListItem(file, img, croppedImageURL, blob) {
+  const listItem = document.createElement("li");
+  const croppedImage = document.createElement("img");
+  const imageInfoContainer = document.createElement("div");
+  const imageSizeInfo = document.createElement("p");
+  const imageNameInfo = document.createElement("p");
+  imageNameInfo.classList.add("image-name");
+  imageNameInfo.innerText = file.name;
+  imageSizeInfo.innerText = `${img.width}x${img.height}px → ${cropSize.width}x${cropSize.height}px`
+  croppedImage.src = croppedImageURL;
+  imageInfoContainer.appendChild(imageNameInfo);
+  imageInfoContainer.appendChild(imageSizeInfo);
+  listItem.appendChild(croppedImage);
+  listItem.appendChild(imageInfoContainer);
+
+  const downloadButton = document.createElement("button");
+  downloadButton.classList.add("download-button");
+  const downloadIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const downloadIconPath1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  const downloadIconPath2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+  downloadIcon.setAttribute("viewBox", "0 0 29.978 29.978");
+  downloadIcon.setAttribute("width", "20");
+  downloadIcon.setAttribute("height", "20");
+  downloadIconPath1.setAttribute("d", "M25.462,19.105v6.848H4.515v-6.848H0.489v8.861c0,1.111,0.9,2.012,2.016,2.012h24.967c1.115,0,2.016-0.9,2.016-2.012v-8.861H25.462z");
+  downloadIconPath2.setAttribute("d","M14.62,18.426l-5.764-6.965c0,0-0.877-0.828,0.074-0.828s3.248,0,3.248,0s0-0.557,0-1.416c0-2.449,0-6.906,0-8.723c0,0-0.129-0.494,0.615-0.494c0.75,0,4.035,0,4.572,0c0.536,0,0.524,0.416,0.524,0.416c0,1.762,0,6.373,0,8.742c0,0.768,0,1.266,0,1.266s1.842,0,2.998,0c1.154,0,0.285,0.867,0.285,0.867s-4.904,6.51-5.588,7.193C15.092,18.979,14.62,18.426,14.62,18.426z");
+  downloadIcon.appendChild(downloadIconPath1);
+  downloadIcon.appendChild(downloadIconPath2);
+  downloadButton.appendChild(downloadIcon);
+  downloadButton.onclick = () => downloadImage(blob, file.name);
+  listItem.appendChild(downloadButton);
+  downloadAllButton.classList.remove('hidden');
+
+  previewList.appendChild(listItem);
 }
 
 function downloadImage(blob, fileName) {
